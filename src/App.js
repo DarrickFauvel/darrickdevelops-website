42176be25283bd6f60b0a5dee707d59374abbf7c
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2023-01-05 10:14:40 +0000

    Add favicon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Darrick Develops
-        </h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Darrick Develops</h1>
         <p>website coming soon</p>
       </header>
     </div>
